ef2f7c0d750b2cc557b7f3d880aeef2f5cee4a6f
Evgeni B <eujeni@list.ru>
2019-01-03 14:05:14 +0300

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+	v-content
+		v-container(fluid  d-block)
+			v-layout(row wrap)
+				v-flex.headline(tag="h3") The title of the project 2
+</template>
+
+<script>
+
+export default {
+  name: 'home'
+}
+</script>
